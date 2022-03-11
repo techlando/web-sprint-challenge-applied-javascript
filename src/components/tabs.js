63e1c092e3045d3a1f20006129bf3a589b34806b
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-03-11 17:52:24 -0600

    sprint challenge finished tasked 1-6

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -16,28 +16,33 @@ const Tabs = (topics) => {
   // </div>
   //
 
-  const topics1 = document.createElement('div');
+  const firstClass = document.createElement('div');
   const tab1 = document.createElement('div');
   const tab2 = document.createElement('div');
   const tab3 = document.createElement('div');
+  const tab4 = document.createElement('div');
+  const tab5 = document.createElement('div');
 
-  topics1.classList.add('topics');
+  firstClass.classList.add('topics');
   tab1.classList.add('tab');
   tab2.classList.add('tab');
   tab3.classList.add('tab');
+  tab4.classList.add('tab');
+  tab5.classList.add('tab');
 
   tab1.textContent = topics[0];
   tab2.textContent = topics[1];
   tab3.textContent = topics[2];
+  tab4.textContent = topics[3];
+  tab5.textContent = topics[4];
 
 
-  topics1.appendChild(tab1);
-  topics1.appendChild(tab2);
-  topics1.appendChild(tab3);
+  firstClass.append(tab1, tab2, tab3, tab4, tab5);
   
 
-  return topics1;
+  return firstClass;
 }
+
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -46,21 +51,20 @@ const tabsAppender = (selector) => {
   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  const entryPoint = document.querySelector(selector);
   axios.get('http://localhost:5000/api/topics')
   .then(res => {
-    console.log(res);
-    res.data.topics.forEach(item => {
-    const topicCard = Tabs(item)
-    entryPoint.appendChild(topicCard)
-    })
+    document.querySelector(selector).appendChild(Tabs(res.data.topics))
+    
   })
   .catch(err => {
-    console.error(err);
+    console.error(err)
   })
-  .finally(() => {
-    console.log('its working, its working!')
-  })
+  
 }
 
 export { Tabs, tabsAppender }
+
+
+
+
+
